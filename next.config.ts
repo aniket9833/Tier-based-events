fbb74e730219293a988668842c04708219c7380a
Aniket Ingole <aniketingolenet@gmail.com>
2025-08-02 23:36:46 +0530

    Added images url domains for event cards

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "media.istockphoto.com",
+      // add more domains as needed, e.g. "images.unsplash.com"
+    ],
+  },
 };
 
 export default nextConfig;
